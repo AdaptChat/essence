2d980297867d3df898339485fb5a7c50fab7bf8b
jay3332 <40323796+jay3332@users.noreply.github.com>
2023-05-29 22:10:20 -0400

    fix attachments migrations

--- migrations/20230530002154_message_attachments.up.sql
+++ migrations/20230530002154_message_attachments.up.sql
@@ -1,7 +1,12 @@
 CREATE TABLE IF NOT EXISTS attachments (
     id UUID NOT NULL PRIMARY KEY,
-    message_id BIGINT NOT NULL REFERENCES messages(id) ON DELETE CASCADE,
+    message_id BIGINT NOT NULL,
+    revision_id BIGINT NOT NULL,
     filename TEXT NOT NULL,
     size BIGINT NOT NULL,
-    alt TEXT
+    alt TEXT,
+    CONSTRAINT fk_attachments_messages
+        FOREIGN KEY (message_id, revision_id)
+        REFERENCES messages(id, revision_id)
+        ON DELETE CASCADE
 );
