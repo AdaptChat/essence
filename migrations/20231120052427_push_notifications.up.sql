f935c26ce801ec5d50162e47d8800f377f4e92dc
Jay3332 <40323796+jay3332@users.noreply.github.com>
2023-11-20 13:27:02 +0000

    fix the crazy SQL errors from cryptex

--- migrations/20231120052427_push_notifications.up.sql
+++ migrations/20231120052427_push_notifications.up.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS push_registration_keys (
     user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     registration_key TEXT NOT NULL PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-)
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
-CREATE FUNCTION IF NOT EXISTS delete_stale_keys() RETURNS trigger
+CREATE OR REPLACE FUNCTION delete_stale_keys() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
 DECLARE row_count INT;
 BEGIN
     DELETE FROM push_registration_keys
-    WHERE created_at < NOW() - INTERVAL '2 days';
+    WHERE created_at < CURRENT_TIMESTAMP - INTERVAL '2 days';
     IF found THEN
         GET DIAGNOSTICS row_count = ROW_COUNT;
         RAISE NOTICE 'DELETED % row(s) FROM push_registration_keys', row_count;
