43e930a5974ca9e14b67217f5a979f22bd87b0eb
jay3332 <40323796+jay3332@users.noreply.github.com>
2023-12-01 15:02:53 -0500

    catch exception

--- migrations/20230225032613_relationships.up.sql
+++ migrations/20230225032613_relationships.up.sql
@@ -1,9 +1,13 @@
-CREATE TYPE relationship_type AS ENUM (
-    'friend',
-    'blocked', -- user blocked target
-    'incoming', -- target has sent a friend request to user
-    'outgoing' -- user has sent a friend request to target
-);
+DO $$ BEGIN
+    CREATE TYPE relationship_type AS ENUM (
+        'friend',
+        'blocked', -- user blocked target
+        'incoming', -- target has sent a friend request to user
+        'outgoing' -- user has sent a friend request to target
+    );
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
 
 CREATE TABLE IF NOT EXISTS relationships (
     user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
