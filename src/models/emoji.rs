17486d668326eedf96c0ef5eb1be49b5c43e2bc3
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2023-12-25 14:39:17 -0800

    Flip Deser Ser in emoji model

--- src/models/emoji.rs
+++ src/models/emoji.rs
@@ -4,8 +4,8 @@ use serde::Serialize;
 #[cfg(feature = "utoipa")]
 use utoipa::ToSchema;
 
-#[derive(Clone, Debug, Deserialize, PartialEq, Eq)]
-#[cfg_attr(feature = "client", derive(Serialize))]
+#[derive(Clone, Debug, Serialize, PartialEq, Eq)]
+#[cfg_attr(feature = "client", derive(Deserialize))]
 #[cfg_attr(feature = "utoipa", derive(ToSchema))]
 #[cfg_attr(feature = "bincode", derive(bincode::Encode, bincode::Decode))]
 pub struct Emoji {
